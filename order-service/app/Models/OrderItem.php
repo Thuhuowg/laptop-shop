10619b42fc8ed1aefe8ef833b46987c1ca886ba5
Thu Hương <huogthu552@gmail.com>
2024-12-20 10:45:09 +0700

    huog's first commit

---
+++ order-service/app/Models/OrderItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Order;
+
+class OrderItem extends Model
+{
+    //
+    use HasFactory;
+    use SoftDeletes;
+    protected $fillable = [
+        'oder_id',
+        'order_item_id',
+        'product_name',
+        'quantity',
+        'price',
+    ];
+    public function order () {
+        return $this->belongTo(Order::class);
+    }
+}
--- order-service/app/Models/User.php
+++ order-service/app/Models/User.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;  // Thêm dòng này
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject  // Implement JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -18,9 +17,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'phone_number',
+        'address'
     ];
 
     /**
@@ -42,4 +43,24 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Lấy định danh (ID) của người dùng.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();  // Hoặc có thể là $this->id nếu khóa chính là id
+    }
+
+    /**
+     * Lấy các thuộc tính tùy chỉnh của người dùng (thêm vào trong token).
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];  // Có thể trả về mảng các thuộc tính tùy chỉnh nếu cần
+    }
 }
