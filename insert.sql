988146099ec3747c145af1f0925e25d187a9a530
Thu Hương <huogthu552@gmail.com>
2024-12-23 10:22:33 +0700

    add products

---
+++ insert.sql
@@ -0,0 +1,94 @@
+SELECT * FROM `transactions` WHERE 1;
+insert into products (product_id, product_name, description, price, category_id, discount_id, image_url)
+VALUES ('P51', 'LENOVO THINKPAD P51', 'Đặc điểm nổi bật
+Địa chỉ bán Laptop Lenovo Thinkpad P51 uy tín trên toàn quốc - LaptopAZ.vn
+ 
+Lenovo Thinkpad P51 dòng máy workstattion chuyên làm đồ hoạ 3D, làm Fim 4k, các phần mềm chỉnh sửa anh chuyên nghiệp. 
+ 
+Với nhu cầu sử dụng máy trạm đồ họa hiệu suất cao cho công việc Thiế kế đồ họa 3D hoặc xử lý các Video 4K thì Lenovo ThinkPad P51 là sự lựa chọn không thể bỏ qua. Thế hệ máy trạm tiếp theo của Lenovo ThinkPad được trang bị CPU Intel Core i7-7820HQ  và Card đồ họa Nvidia Quadro  M2200M . Lenovo ThinkPad P51 là dòng Workstation 15.6" thực sự nổi bật với Cấu hình mạnh mẽ, Thiết kế chắc chắn và chất lượng bàn phím số 1 trên thế giới.
+Chiếc máy tính Lenovo Thinkpad P51 có thiết kế nhẹ và ấn tượng nhất trong các mẫu laptop máy trạm di động mobile workstation với các thích thước các chiều 377x252x25 cùng cân nặng khoảng từ 2.5 kg – 2.9 kg.
+Thinkpad P51 vẫn sử dụng bộ khung nhôm, magie đây là kết cấu bền bỉ và ổn định của hãng Lenovo được sử dụng trong nhiều sản phẩm dòng thinkpad cùng với đó là nắp máy sử dụng sợi carbon, tất cả đều phải trải qua quá trình test kiểm tra độ bền theo tiêu chuẩn quân sự Mỹ MIL-STD 810G về độ ẩm, độ rung lắc, khả năng chống sốc và chịu nhiệt.
+Màn hình chi tiết, màu sắc rực rỡ
+Với tùy chọn cao cấp nhất Thinkpad P51 đem lại trải nghiệm hình ảnh rất tốt với độ phân giải FULL HD của mình. Hình ảnh trong các bộ phim 4K được hiện thị rất chi tiết cùng với đó là màu sắc và tương phản tốt, người xem có thể thấy rõ được các phân vùng nhỏ trên màn ảnh.
+Khả năng tái tạo màu trên màn của thinkpad P51 có thể lên tới hơn 170% gam màu sRGB cực kỳ ấn tượng hơn nhiều so với một số đối thủ như Macbook Pro và Dell Precision 5520, thậm chí với cảm biến và phần mềm X Rite Pantone thì người mua có thể tùy biến chất lượng màu sắc một cách chính xác.
+Cấu hình cao cấp 
+Theo Geekbench 4 thì Thinkpad P51 có thể đạt tới hơn 15 nghìn điểm cao hơn nhiều so với mức trung bình. Thử nghiệm ghép các marco trên excel với 20000 tên với địa chỉ mất khoảng 3 phút cao hơn các mốc thời gian trung bình.
+Sức mạnh của SSD NVMe PCIe hiện nay có thể gọi là vô đối nhất khi chỉ khoảng 10 giây bạn đã có thể sao chép xong file nặng khoảng 5GB data dữ liệu tất cả chỉ như một cái chớp mắt.
+Bàn phím dần thay đổi với nhiều cải tiến
+Có một chút thay đổi về thiết kế bàn phím trên những mẫu laptop đời mới dòng thinkpad và Thinkpad P51 cũng sẽ như vậy, có vẻ cần một chút thời gian để người mua quen với nó khi mà những trải nghiệm cũ đã quá ấn tượng. Tuy vậy xét trên thị trường vẫn rất có có mẫu laptop nào đem lại trải nghiệm gõ phím thích như trên Thinkpad P51 nói riêng và dòng thinkpad nói chung.
+Sử dụng track point màu đỏ là điều thú vị nhất trên thinkpad P51 tất nhiên với nhưng ai mới mua dung thinkpad thì hoàn toàn có thể sử dụng touchpad phía dưới bàn phím với cảm giác mượt, chính sách, độ nẩy của các phím chuột rất thích tay.
+Các kết nối ngoại vi mới nhanh hơn
+Việc loại bọ những kết nối cũ đang trở thành xu hướng mà nhiều hãng đã tiến hành và Thinkpad P51 cũng sẽ như vậy khi được trang bị các kết nối thế hệ mới như Mini Displayport, usb 3.0, thunderbolt, hdmi, usb type c và không có sự xuất hiện của ổ DVD.
+Thời lượng pin khá tốt
+Thinkpad P51 mặc dù có màn hình có độ phân giải cao và các thành phần đòi hỏi sức mạnh, tuy nhiên vẫn có thể sử dụng trong khoảng 4 tiếng đồng hồ với pin 6 cell hơn khá nhiều so với các mẫu laptop khác.
+Tổng kết
+ThinkPad P51 kết hợp hiệu suất tổng thể và đồ họa với màn hình đầy màu sắc, thiết kế bền và thời lượng pin khá tốt. Nếu bạn đang tìm kiếm một máy tính xách tay xách tay có cấu hình cao dễ dàng cho các công việc chuyên nghiệp như lập trình, thiết kế 3d hay làm kiến trúc thì một lựa chọn cấu hình như Lenovo Thinkpad P51 là rất đáng lưu tâm.
+',22890000.00, 2, 1, 'thinkpadp51'),
+('T490','ThinkPad T490', 'Cảm nhận đầu tiên về Laptop Lenovo Thinkpad t490 đó là phong cách thiết kế có nhiều nét giống với những người anh, em tiền nhiệm trước đó về kiểu dáng nhưng lại có độ mỏng và tính di chuyển đáng ngưỡng mộ. Máy được làm bằng chất liệu cacbon đen sang trọng, nổi bật trên nắp máy là Logo ThinkPad màu trắng và có chấm đỏ làm điểm nhấn cho máy. Vẫn giữ được công nghệ bàn phím làm hai lỏng rất nhiều khách hàng khó tình nhất!
+Laptop Lenovo Thinkpad t490 máy mới 100%. LaptopAZ.vn có chế độ bảo hành sau bán hàng hấp dẫn, bảo hành 12 tháng phần cứng, bào hành phần mềm trọn đời, bao test đổi trả trong 15 ngày đầu nên khách hàng yên tâm khi mua hàng.
+Mỏng nhẹ và tinh tế
+Với trọng lượng chỉ từ 1.27kg, ThinkPad t490 có tính di động rất cao. Viền màn hình vát kim cương được thiết kế nhỏ lại để khiến diện tích hiển thị lớn hơn nhiều so với các mẫu trước đó và một loạt những tùy chọn màn hình như màn hình cảm ứng, tích hợp tính năng PrivacyGuard và cả công nghệ hiển thị Dolby Vision®. Thời lượng sử dụng pin sẽ cao hơn và tính năng kết nối toàn cầu LTE-A sẽ khiến cho chiếc laptop này trở thành một chiếc laptop doanh nhân cao cấp.
+Màn hình tuyệt đẹp
+Viền màn hình trên t490 được thu nhỏ lại giúp cho diện tích hiển thị được tăng lên đáng kể, trông như màn hình 13 inch nhưng có khả năng hiển thị của màn hình 14inch.
+ 
+Công nghệ tần nhạy sáng cao (HDR) Dolby Vision® p được tích hợp trên màn hình 2K WQHD sẽ giúp cho độ sáng được tăng lên rất nhiều, mở rộng tỉ lệ độ tương phản, làm cho màu sắc trở nên siêu chuẩn, các chi tiết sẽ trở nên thực hơn. Kết hợp với card đồ họa rời và T490 sẽ trở nên rất thích hợp cho những người làm về Multimedia (đa phương tiện). Hoặc bạn cũng có thể chọn màn hình FHD giúp tiết kiệm điện năng và tăng thời lượng pin lên khà nhiều.
+ 
+Bền bỉ trong mọi môi trường
+ 
+Vượt qua 12 bài kiểm tra tiêu chuẩn quân đội về độ bền và hơn 200 lần kiểm tra về khả năng làm việc trong môi trường khắc nghiệt: Dù là Bắc Cực hay bão cát sa mạc, dù là không trọng lực hay va đập thì cũng không là gì so với t490.
+
+Nghe nhạc chân thật
+Công nghệ âm thanh vòm Dolby Audio™ Premium  cho phép âm thanh phát to và rõ hơn, thậm chí bạn còn có thể nghe được từng từ khi nghe Opera. Âm thanh siêu thật và sống động hệt như trong rạp chiếu phim giúp bạn có những giây phút giải trí tuyệt vời.
+Khả năng sạc nhanh siêu tốc
+t490 có thể trụ đến 20 giờ làm việc liên tục và nếu sắp hết pin, công nghệ sạc siêu nhanh (Rapid Charge) sẽ giúp bạn hồi phục đến 80% lượng pin chỉ trong 1 giờ.
+
+Luôn luôn kết nối
+Dù bạn có ở ngoài không gian hay đang ở trong bàn làm việc, t490 luôn kết nối với bạn. Card kết nối toàn cầu LTE-A có khả năng kết nối đến bất cứ nơi nào trên thế giới.
+ 
+Bảo mật cao hơn
+ 
+Tùy chọn Camera hồng ngoại IR và bộ cảm ứng vân tay sẽ hỗ trợ bạn đăng nhập bằng phương pháp sinh trắc một cách nhanh chóng mà không phải nhập mật khẩu.
+Kết nối mạng nhanh hơn tới 6 lần
+Card WiFi Intel® Dual-Band 9560 802.11AC (2 x 2)  có tốc độ tối đa lên tới 1.73Gbs, nhanh hơn gần 6 lần so với Card WiFi chuẩn N phổ thông hiện nay. Cho phép bạn thoải mái kết nối mạng không dây dù đang ở cách xa đến một dãy nhà.
+', 22890000.00,2, 1, 'thinkpadt490'),
+('14IML9', 'Lenovo Yoga 7 2-in-1', 'Đặc điểm nổi bật
+Lenovo Yoga 7 2-in-1 - Cảm ứng mỏng nhẹ& thiết kế đỉnh cao
+Lenvo Yoga 7 2-in-1 có thể nói là phiên bản hoàn hảo nhất của dòng laptop mỏng nhẹ tới từ nhà Lenovo bởi thiết kế sang trọng cùng trọng lượng chỉ 1.6kg. Với một thân hình mảnh mai, Lenovo Yoga 7 2-in-1 mang trong mình cấu hình Intel® Core™ Ultra 5 125U. Chưa hết, Lenovo Yoga 7 2-in-1 còn được trang bị màn hình FHD+ cảm ứng đẹp mắt, hứa hẹn sẽ mang đến cho bạn trải nghiệm tuyệt vời. Hãy cùng LaptopAZ đi tìm hiểu nhé! 
+Thiết kế
+Lenovo Yoga 7 2-in-1 đi theo lối thiết kế tinh tế và hiện đại. Vỏ kim loại màu xám đen tạo nên một ngoại hình sang trọng và lịch lãm, đồng thời mang tới sự bền bỉ và khả năng tản nhiệt hiệu quả. Đường cắt vát trên bề mặt máy tạo nên các góc cạnh và tạo hiệu ứng ánh sáng vô cùng độc đáo. Lenovo Yoga 7 2-in-1 sở hữu sự kết hợp tuyệt vời giữa Thiết kế này thể hiện sự kết hợp tuyệt vời giữa thẩm mỹ và hiệu suất làm việc. 
+Lenovo Yoga 7 2-in-1 có kích thước 317,72 x 222,13 x 16,64mm và có trọng lượng khoảng 1.6 kg. Điều này làm cho máy trở nên nhẹ và mỏng hơn so với nhiều laptop cùng phân khúc, đặc biệt nhờ sử dụng nhôm và việc thiết kế màn hình với viền mỏng. Điều này cho phép máy dễ dàng vừa vặn trong balo hoặc túi xách của bạn mà không tạo cảm giác nặng nề hay cồng kềnh. Máy cũng có một bản lề cho phép mở màn hình ở góc lên đến 180 độ, giúp bạn điều chỉnh góc nhìn một cách thuận tiện.
+Màn hình
+Điểm nhấn ấn tượng nhất với người dùng trên con máy Lenvo Yoga 7 2-in-1 chính là màn hình. Màn hình của Lenovo Yoga 7 2-in-1 có kích thước 14", độ phân giải FHD+ (1920x1200) được hiệu chuẩn màu sắc để cho hình ảnh chân thực nhất. Màn hình này có công nghệ IPS cho góc nhìn rộng và độ sáng tối đa lên đến 300 nits. Màn hình cũng có tính năng cảm ứng và xoay gập 360 độ, giúp người dùng có thể thao tác trực tiếp trên màn hình một cách dễ dàng và tiện lợi. Ngoài ra Lenovo Yoga 7 2-in-1 với công nghệ xử lý hình ảnh hiện đại còn mang tới trải nghiệm không gian thực với hình ảnh sắc nét cho người dùng, đáp ứng nhu cầu thiết kế, chỉnh video không thua kém các máy tính chuyên về đồ họa.
+Một điểm cộng nữa của màn hình của Yoga 7 2-in-1 là viền màn hình rất mỏng, giúp người dùng dễ dàng đắm chìm trong từng khung hình, tạo sự thoải mái khi làm việc hoặc chơi game. Màn hình này cũng được bảo vệ bởi kính cường lực, giúp chống trầy xước và va đập.
+Hiệu năng
+Không khó khi người dùng có thể nhận thấy rằng với một thiết kế bề ngoài hoàn mỹ thì CPU đời mới hiện đại cũng được trang bị trên chiếc Lenovo Yoga 7 2-in-1. Với việc được trang bị con chip CPU Intel® Core™ Ultra 5 125U (1.30GHz up to 4.30GHz, 12MB Cache), đi cùng Ram 16GB LPDDR5x-7467MHz và 512GB SSD NVMe PCIe, Lenovo Yoga 7 2-in-1 có thể thoải mái cân tất cả các tác vụ công việc cũng như hoạt động đa nhiệm mượt mà. 
+Ngoài ra, Lenovo Yoga 7 2-in-1 được trang bị card đồ họa tích hợp cung cấp khả năng xử lý hình ảnh tốt để đáp ứng các công việc hàng ngày, chơi game và giải trí. 
+Bàn phím - Touchpad
+Bàn phím trên Yoga 7 2-in-1 có thiết kế kiểu chiclet với các phím được đặt khá rộng rãi, tạo cảm giác thoải mái khi gõ. Các phím có hành trình hợp lý và độ nảy tốt, giúp tăng tốc độ gõ phím và giảm thiểu sai sót. Ngoài ra, bàn phím cũng được trang bị đèn nền, cho phép người dùng làm việc trong môi trường thiếu ánh sáng.
+Cổng kết nối
+Cũng giống như phiên bản tiền nhiệm Lenvo Yoga 7 2-in-1 được trang bị đầy đủ các cổng kết nối giúp cho người dùng thuận tiện kết nói với các thiết bị ngoại vi như: chuột, bàn phím, USB, Box HDD,… một cách thoải mái đem lại trải nghiệm tốt nhất trong quá trình sử dụng. Hệ thống cổng kết nối bao gồm: 1x USB-A (USB 5Gbps / USB 3.2 Gen 1), 2x USB-C® (Thunderbolt™ 4 / USB4® 40Gbps), 1x HDMI® 2.1, 1x Giắc cắm kết hợp tai nghe / micrô (3,5 mm), 1x đầu đọc thẻ nhớ microSD
+Ngoài ra Lenovo Yoga 7 2-in-1 còn là một trong những chiếc laptop có thời lượng pin tốt trên thị trường hiện nay. Với pin có dung lượng lên đến 71Wh, máy có thể sử dụng liên tục lên đến trên 10 giờ trên một lần sạc đầy, tuỳ thuộc vào cấu hình và cách sử dụng của người dùng. 
+',17990000.00, 2, 1, 'yoga7'),
+('G6','Lenovo Thinkbook 14 G6+ 2024','Đặc điểm nổi bật
+Laptop Lenovo ThinkBook 14 G6+ có đáng để sở hữu?
+Lenovo ThinkBook 14 G6+ là chiếc laptop doanh nhân mạnh mẽ nhưng đầy phong cách. Bên cạnh bộ vi xử lý thế hệ mới nhất, ThinkBook 14 G6+ còn tích hợp những tính năng thông minh để tối ưu hóa cho khả năng làm việc từ xa vốn đang là xu hướng hiện nay.
+Tăng cường năng suất làm việc nhờ cấu hình tiên tiến
+Lenovo ThinkBook 14 G6+ có sức mạnh vượt trội nhờ việc sử dụng bộ vi xử lý Intel Ultra 5 125H mới nhất hiện nay. Con chip Intel Ultra 5 125H hiệu năng cao không chỉ cho tốc độ nhanh với 14 lõi 18 luồng, tốc độ xung nhịp tối đa lên tới 4.50GHz. Kết hợp thêm 16GB LPDDR5x 7467MT/s và ổ cứng SSD dung lượng cao 512GB, bạn sẽ có một thiết bị làm việc lý tưởng, tối ưu hiệu suất trong công việc.
+Khả năng xử lý đồ họa xuất sắc
+Bộ vi xử lý Intel Ultra 5 125H tích hợp GPU đồ họa Intel Arc Graphics mạnh mẽ hàng đầu hiện nay. Nhờ sức mạnh đồ họa ấn tượng, Lenovo ThinkBook 14 G6+ có thể chỉnh sửa ảnh, biên tập video hay chạy các ứng dụng đồ họa cơ bản, cho bạn làm được nhiều việc hơn trên chiếc laptop di động.
+Thiết kế sang trọng và chuyên nghiệp
+Lenovo ThinkBook 14 G6+ mang đến cho bạn nhiều cảm hứng làm việc hơn nhờ một thiết kế sang trọng và chuyên nghiệp. Phần viền màn hình siêu mỏng, kích thước gọn nhẹ, di động và một thiết kế rất thời thượng tạo nên chiếc máy tính xách tay đẹp đẳng cấp. Không chỉ tạo phong cách riêng, sự mỏng nhẹ của Lenovo ThinkBook 14 G6+ còn giúp bạn có thể dễ dàng mang laptop đi bất cứ đâu.
+Màn hình sống động sắc nét
+Màn hình của Lenovo ThinkBook 14 G6+ có kích thước 14 inch, độ phân giải 2K+ sắc nét, cho hình ảnh sống động như thật với màu sắc, độ tương phản chính xác. Viền màn hình siêu mỏng giúp bạn hoàn toàn tập trung vào nội dung trước mắt. Hơn nữa, màn hình này còn có công nghệ lọc ánh sáng xanh bảo vệ mắt đã được chứng nhận bởi TUV Rheinland, giúp bạn làm việc thoải mái dù trong thời gian dài.
+Tối ưu cho khả năng làm việc từ xa
+Những cuộc họp video đang là điều không còn xa lạ hiện nay, khi bạn vẫn có thể tham gia họp mà không cần phải mất thời gian di chuyển đến phòng họp, đồng thời tạo kết nối từ xa trong công việc. Lenovo ThinkBook 14 G6+ hiểu điều đó và đã xây dựng hệ thống webcam sắc nét, âm thanh trung thực cùng micro chống ồn. Bạn sẽ có được chất lượng cuộc gọi tối ưu nhất để công việc hoàn toàn trôi chảy. 
+Khả năng kết nối ấn tượng
+Lenovo ThinkBook 14 G6+ đảm bảo khả năng kết nối linh hoạt và nhanh chóng nhất. Chuẩn WiFi 6 cho tốc độ tải xuống nhanh hơn, băng thông rộng hơn và độ trễ thấp hơn. Nhờ những cổng kết nối tiên tiến hàng đầu, Lenovo ThinkBook 14 G6+ có thể kết nối với bất kỳ thiết bị ngoại vi nào bạn muốn, phục vụ tối đa cho công việc.
+Ngoài ra Lenovo ThinkBook 14 G6+ còn sở hữu số lượng cổng kết nối đầy đủ, đáp ứng được hầu hết các tác vụ trong công việc bao gồm: 1x USB type C, 1x HDMI 2.1, 1x Thunderbolt, 1x Headphone / mic combo 3.5, 2x USB type A, 1x TGX, 1x Đầu đọc thẻ SD, 1x RJ45
+Bảo mật, an toàn và thông minh
+Lenovo ThinkBook 14 G6+ đảm bảo cho dữ liệu và quyền riêng tư cá nhân của bạn luôn an toàn. Các tính năng phần cứng đều được bảo mật bởi nền tảng TPM, kiểm soát truy cập cổng kết nối, tính năng xóa an toàn, … Một màn trập trên webcam giúp bạn tránh khỏi nguy cơ bị truy cập hình ảnh trái phép khi không sử dụng. Ngoài ra laptop cũng tích hợp các tính năng thông minh về khởi động; chế độ bảo vệ mắt; làm mát thông minh. Bạn sẽ có được những trải nghiệm an toàn và thoải mái nhất trên Lenovo ThinkBook 14 G6+.
+Bàn phím tuyệt vời
+Bàn phím của Thinkbook 14 G6+ 2024 với hành trình phím 1.5mm không chỉ mang lại cảm giác gõ phím chính xác mà còn tạo trải nghiệm thoải mái rất đặc biệt. Hành trình phím lớn cũng giúp giảm mệt mỏi trong quá trình sử dụng lâu dài, hòa mình vào không gian làm việc một cách dễ dàng và thoải mái hơn bao giờ hết.
+Touchpad của chiếc laptop này có kích thước lớn mang đến không gian rộng rãi cho những thao tác đa chạm và di chuyển linh hoạt trên màn hình. Mượt mà và chính xác, touchpad giúp người dùng dễ dàng thực hiện các thao tác như cuộn, phóng to, xoay.
+Bên cạnh đó Thinkbook 14 G6+ 2024 sở hữu pin vô cùng mạnh mẽ, với dung lượng lên tới 85Whr. Dung lượng pin lớn này hứa hẹn mang lại thời lượng sử dụng ấn tượng, giúp người dùng thoải mái sử dụng máy tính trong thời gian dài mà không cần lo lắng về việc sạc pin liên tục.
+Tổng kết: Laptop Lenovo ThinkBook 14 G6+ với hiệu năng mạnh mẽ cùng thiết kế thanh lịch, là sự lựa chọn phù hợp cho hầu hết tất cả mọi người, nhất là những ai có nhu cầu học tập - văn phòng. Hiện tại LaptopAZ đang có những chương trình ưu đãi vô cùng tuyệt vời khi khách hàng đặt mua Lenovo ThinkBook 14 G6+ ngay từ ngày hôm nay!' ,18690000.00, 2, 1, 'thinkbook14');
